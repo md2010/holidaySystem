b3ec08b6689a9ce1c2a29b2a69f13fe632cb916f
md2010 <mdamjanovic2010@gmail.com>
2021-03-15 19:51:17 +0100

    commit

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,31 +7,17 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-    /**
-     * The Artisan commands provided by your application.
-     *
-     * @var array
-     */
     protected $commands = [
         //
     ];
 
-    /**
-     * Define the application's command schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-     * @return void
-     */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->call(function () {
+            DB::table('users')->update(['availableDays' => 20]);
+        })->yearly();
     }
 
-    /**
-     * Register the commands for the application.
-     *
-     * @return void
-     */
     protected function commands()
     {
         $this->load(__DIR__.'/Commands');
